c2b968ee0ab35450d702f7e83325c018bcc3c10a
Ebubekir Karanfil <karanfilebubekir@hotmail.com>
2019-10-26 16:29:17 +0300

    start shop app

---
+++ ShopApp.Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
