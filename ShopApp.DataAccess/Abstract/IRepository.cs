f3ca704666d009f07717f52bb870afdeee096466
Ebubekir Karanfil <karanfilebubekir@hotmail.com>
2019-10-27 15:20:43 +0200

    Repository Pattern

---
+++ ShopApp.DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ShopApp.DataAccess.Abstract
+{
+	public interface IRepository<TEntity>
+	{
+		TEntity GetById(int id);
+		TEntity GetOne(Expression<Func<TEntity, bool>> filter);
+		IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null);
+
+		void Create(TEntity entity);
+		void Update(TEntity entity);
+		void Delete(TEntity entity);
+	}
+}
---
+++ ShopApp.DataAccess/Concrete/EfCore/EfCoreGenericRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ShopApp.DataAccess.Concrete.EfCore
+{
+	public class EfCoreGenericRepository<TEntity, TContext> : IRepository<TEntity>
+		where TEntity : class
+		where TContext : DbContext, new()
+	{
+		public void Create(TEntity entity)
+		{
+			using (var context = new TContext())
+			{
+				context.Set<TEntity>().Add(entity);
+				context.SaveChanges();
+			}
+		}
+
+		public void Delete(TEntity entity)
+		{
+			using (var context = new TContext())
+			{
+				context.Set<TEntity>().Remove(entity);
+				context.SaveChanges();
+			}
+		}
+
+		public IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+		{
+			using (var context = new TContext())
+			{
+				return filter == null
+					? context.Set<TEntity>().ToList()
+					: context.Set<TEntity>().Where(filter).ToList();
+			}
+		}
+
+		public TEntity GetById(int id)
+		{
+			using (var context = new TContext())
+			{
+				return context.Set<TEntity>().Find(id);
+			}
+		}
+
+		public TEntity GetOne(Expression<Func<TEntity, bool>> filter)
+		{
+			using (var context = new TContext())
+			{
+				return context.Set<TEntity>().Where(filter).SingleOrDefault();
+			}
+		}
+
+		public void Update(TEntity entity)
+		{
+			using (var context = new TContext())
+			{
+				context.Entry(entity).State = EntityState.Modified;
+				context.SaveChanges();
+			}
+		}
+	}
+}
