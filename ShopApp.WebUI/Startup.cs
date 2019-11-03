1f18e2002143758c72c83d80d9ee61438e259f63
Ebubekir Karanfil <karanfilebubekir@hotmail.com>
2019-11-03 13:45:00 +0200

    Dependency Injection

--- ShopApp.WebUI/Startup.cs
+++ ShopApp.WebUI/Startup.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using ShopApp.Business.Abstract;
+using ShopApp.Business.Concrete;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.DataAccess.Concrete.Memory;
 
 namespace ShopApp.WebUI
 {
@@ -15,6 +19,11 @@ namespace ShopApp.WebUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddScoped<IProductDal, MemoryProductDal>();
+			services.AddScoped<IProductService, ProductManager>();
+
+			services.AddMvc().
+				SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +34,7 @@ namespace ShopApp.WebUI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+			app.UseMvcWithDefaultRoute();
         }
     }
 }
